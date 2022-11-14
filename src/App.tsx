5c87a7bb117d1dc863a9d8ff44e09327ec3eb0c9
wangerhao <1532531258@qq.com>
2022-11-14 16:16:41 +0800

    引入ant design

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './App.css'
-
+import { Button } from 'antd'
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  )
 }
 
 export default App
